1b79e9380f87e0837accd0aed009de59d610557e
GEsparza21 <99316235+GEsparza21@users.noreply.github.com>
2023-06-10 23:43:41 -0600

    Add files via upload

---
+++ desactivar.js
@@ -0,0 +1,31 @@
+var input = document.getElementById("Nombre");
+var input2 = document.getElementById("Apellido");
+var input3 = document.getElementById("Correo");
+var input4 = document.getElementById("Piso");
+var input5 = document.getElementById("Apartamento");
+var input6 = document.getElementById("Cajon");
+var input7 = document.getElementById("Telefono");
+var btnEdit = document.getElementById("btnedit");
+
+// Deshabilitar el campo de entrada de texto
+input.disabled = true;
+input2.disabled = true;
+input3.disabled = true;
+input4.disabled = true;
+input5.disabled = true;
+input6.disabled = true;
+input7.disabled = true;
+
+//si se preciona el boton editar debe habilitar los campos
+function habilitar(){
+    input.disabled = false;
+    input2.disabled = false;
+    input3.disabled = false;
+    input4.disabled = false;
+    input5.disabled = false;
+    input6.disabled = false;
+    input7.disabled = false;
+}
+
+// Habilitar el campo de entrada de texto
+//input.disabled = false;
